db19599ac35574840db9862e9df0ac32af80f515
Jared Hightower <Jared.jhightower@gmail.com>
2020-04-24 14:39:03 -0500

    Updates

---
+++ precache-manifest.a2dd4099dff7b8b880705ae1e46b2e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a52756f4c75d2d61d2df2f712030b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0339f1ea39b091995ad0",
+    "url": "/static/css/main.ecadd557.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0339f1ea39b091995ad0",
+    "url": "/static/js/main.ab4e58b5.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
